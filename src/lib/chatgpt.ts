e8f2122541aefbe0a501b9d4e715b0f1d062a879
杨乐乐 <youngle316@gmail.com>
2023-06-14 14:16:10 +0800

    build: remove dependency ➖

--- src/lib/chatgpt.ts
+++ src/lib/chatgpt.ts
@@ -1,28 +1,8 @@
 import { ChatGPTAPI } from "chatgpt";
-import { HttpsProxyAgent } from "https-proxy-agent";
-import nodeFetch from "node-fetch";
-
-const isDev = process.env.NODE_ENV === "development";
 
 const api = new ChatGPTAPI({
 	apiKey: (process.env.NEXT_PUBLIC_OPENAI_API_KEY as string) || "",
 	apiBaseUrl: "https://openaiproxy-4bc.pages.dev/v1",
-	// @ts-ignore
-	fetch: isDev
-		? (url, options = {}) => {
-				const defaultOptions = {
-					agent: new HttpsProxyAgent("http://127.0.0.1:7890"),
-				};
-
-				const mergedOptions = {
-					...defaultOptions,
-					...options,
-				};
-
-				// @ts-ignore
-				return nodeFetch(url, mergedOptions);
-		  }
-		: fetch,
 });
 
 export { api };
