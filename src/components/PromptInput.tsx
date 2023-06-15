6cd22e0cbc27b7f001f90986e80b5c6a87f8bf90
杨乐乐 <youngle316@gmail.com>
2023-06-15 10:36:45 +0800

    feat: press 'Enter' to send a message

--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -127,6 +127,13 @@ function PromptInput() {
 		setIsTyping(false);
 	};
 
+	const promptInputKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+		if (e.key === "Enter" && !e.shiftKey) {
+			e.preventDefault();
+			sendPrompt();
+		}
+	};
+
 	return (
 		<div className="fixed pb-4 px-4 z-30 bottom-0 left-0 right-0 transition-all duration-300 lg:pl-80 ">
 			<div className="mx-auto w-full transition-all max-w-5xl px-4 md:px-8 lg:px-12">
@@ -140,6 +147,7 @@ function PromptInput() {
 						onChange={chatTextAreaChange}
 						placeholder={t("promptInputPlaceholder")}
 						value={inputPrompt}
+						onKeyDown={promptInputKeyDown}
 					/>
 					<button
 						type="button"
